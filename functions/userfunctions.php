cc8c9f111a56014c04581a04fb78f2952b083a00
Angela Lin Ding <angela.ding3@gmail.com>
2015-03-18 22:39:04 -0500

    connected login form to home page

--- functions/userfunctions.php
+++ functions/userfunctions.php
@@ -6,7 +6,7 @@ function logged_in() {
 function prevent_intruders() {
 	if (!logged_in()) {
 		echo '<h4>Sorry, you must be logged in to access the contents of this page.</h4>
-		<h4>Please <a href="loginform.php">log in</a>.</h4>';
+		<h4>Please <a href="index.php">return home</a> and log in or register for an account.</h4>';
 		exit();
 	}
 }
