9b537527b8890549134c1e7ae2e3aca74d4d3f2d
Angela Lin Ding <angela.ding3@gmail.com>
2015-03-18 21:00:00 -0500

    added registration successful feedback

--- db/userfunctions.php
+++ db/userfunctions.php
@@ -1,10 +1,4 @@
 <?php
 
-function login($username, $password) {
-	$username = sanitize($username);	
-	$password = md5($password);
-	$query = $db->query("SELECT COUNT(*) FROM users WHERE username='$username' AND password='$password'");
-	return (mysql_result($query,0) == 1) ? $username : false;
-}
 
 ?>
--- loginform.php
+++ loginform.php
@@ -1,3 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>wikiMenus</title>
+  <script src="./lib/angular/angular.min.js"></script>
+  <script src="./js/controllers.js"></script>
+  <link rel="stylesheet" href="./css/styles.css">
+  <link rel="stylesheet" href="./foundation/css/foundation.css">
+  <script src="http://code.jquery.com/jquery-latest.js"></script>
+  <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+  <link href="http://cdnjs.cloudflare.com/ajax/libs/foundicons/3.0.0/foundation-icons.css" rel="stylesheet">
+  <link rel="shortcut icon" href="data/pics/favicon.ico" type="image/x-icon" />  
+  <link href="http://fonts.googleapis.com/css?family=Rancho" rel="stylesheet" type="text/css">
+  <link href="http://fonts.googleapis.com/css?family=Gudea" rel="stylesheet" type="text/css">
+  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <?php require "db/connect.php"; ?>	<!--connect to database-->
+</head>
+<body>
 <?php
 require "db/connect.php"; 
 require "functions/userfunctions.php";
@@ -8,20 +27,22 @@ require "functions/userfunctions.php";
 	<div class="small-12 medium-6 columns">
 		<h1 id="subscribeText">Register</h1>		
 		<?php
+		if(isset($_GET['registered'])) {
+			echo "<p>You have successfully registered for an account.
+					You may now log in on the right.</p>";
+		}
 		if(!empty($_POST)) {
 			if(isset($_POST['email'], $_POST['username'], $_POST['password'])) {
 				$email = trim($_POST['email']);
 				$username = trim($_POST['username']);
 				$password = trim($_POST['password']);
-				if(!empty($email) && !empty($username) && !empty($password)) {
-					echo "<p>You have successfully registered for an account.
-					You may now log in on the right.";
+				if(!empty($email) && !empty($username) && !empty($password)) {					
 					$password = md5($password);
 					$insert = $db->prepare("INSERT INTO users (email, username, password) 
 						VALUES (?, ?, ?)");
 					$insert->bind_param('sss', $email, $username, $password);
 					if($insert->execute()) {
-						header('Location: loginform.php?');
+						header('Location: loginform.php?registered');
 						die();
 					}
 				}
@@ -57,3 +78,6 @@ require "functions/userfunctions.php";
 	</div>
 
 </div>
+
+</body>
+</html>
