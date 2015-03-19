5da26db497664ac5d1ee8f94ff9bfdbc69091ea0
aprilx2222 <aprilxu2222@gmail.com>
2015-03-19 01:16:40 -0500

    blah

--- db/connect.php
+++ db/connect.php
@@ -1,7 +1,7 @@
 <?php
 
 error_reporting(E_ALL);
-$db = new mysqli('127.0.0.1', 'root', '', 'wikimenus');
+$db = new mysqli('127.0.0.1', 'root', '', 'wikimenus', 8000);
 if ($db->connect_errno) {
 	echo $db->connect_error;
 	die('Sorry, we are having problems.');
