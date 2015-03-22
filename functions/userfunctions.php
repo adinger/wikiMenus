6b60b08e1a0b08fb0cfff720cde2e9e6a80832fc
Angela Lin Ding <angela.ding3@gmail.com>
2015-03-21 23:50:56 -0500

    find user's fave restaurant works

--- functions/userfunctions.php
+++ functions/userfunctions.php
@@ -1,13 +1,41 @@
 <?php
 
+function favorite_restaurant($db, $username) {
+    $email = $db->query("SELECT email FROM users WHERE users.username = '$username'");
+    $email = $email->fetch_object();
+    $email = $email->email;
+    $make_view = $db->query("create view restaurantRatings as 
+        (
+            select restaurant, avg(review.numericalrating) as avgrating 
+            from dishreview, dish, review 
+            where dishreview.useremail = '$email' 
+            and dishreview.reviewid = review.reviewid 
+            and dishreview.dishid = dish.dishid 
+            group by dish.restaurant
+        )");
+
+    $result = $db->query("
+        select restaurant
+        from restaurantRatings
+        where avgrating = 
+        ( 
+            select max(avgrating)
+            from restaurantRatings
+        )");
+    $result = $result->fetch_object();
+    return $result->restaurant;
+}
+
 function logged_in() {
 	return isset($_SESSION['username']) ? true : false;
 }
+
 function prevent_intruders() {
 	if (!logged_in()) {
-		echo '<h4>Sorry, you must be logged in to access the contents of this page.</h4>
-		<h4>Please <a href="index.php">return home</a> and log in or register for an account.</h4>';
+		echo '<h4>You must be logged in to access the contents of this page.</h4>
+		<h4>Please <a href="index.php">go to the homepage</a> and log in.</h4>';
 		exit();
 	}
 }
+
 ?>
--- userprofile.php
+++ userprofile.php
@@ -1,13 +1,50 @@
 <?php
-
 include "head.php";
 require "db/connect.php"; 
 include "functions/userfunctions.php";
 prevent_intruders();
 
+$restaurant = favorite_restaurant($db, $_SESSION["username"]);
+
 ?>
 
-<h3>Welcome, <?php echo $_SESSION['username'] ?></h3>
+<script type="text/javascript">
+$(document).ready(function() {
+	var review = $('#review');
+	review.text(review.text().substring(0,250)+' . . . ');
+	/*var str = '<a href="#">Read more</a>',
+	html = $.parseHTML(str);
+	review.append($(html))*/
+});
+</script>
+<div class="row">
+	<div class="small-10 small-centered columns floating">
+		<div class="small-12 medium-4 columns profile-info">
+			<div class="profilepic">
+				<a href="" class="th"><img src="data/pics/profPicPlaceholder.jpg" alt="alt"></a>
+			</div>
+			<h4 class="username-title"><strong><?php echo $_SESSION['username'] ?></strong></h4>
+            <!-- TODO: logout functionality -->
+			<p><strong>Favorite Restaurant:</strong><br><?php echo $restaurant; ?></p>
+			<p><strong>Highest Rated Dish:</strong><br>[favorite dish]</p>
+		</div>
+		<div class="small-12 medium-8 columns">
+			<div class="panel">
+				<h4><strong><?php echo $_SESSION['username'], "'s Reviews" ?></strong></h4>
+				<hr>
+				<div class="usersReview">
+					<h5 class="dishName">Dish: McChicken Sandwich</h5>
+					<h5>Restaurant: McDonald's</h5>
+					<h5>Rating: <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span></h5>
+					<p id="review">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris molestie elit, et lacinia ipsum quam nec dui. Quisque nec mauris sit amet elit iaculis pretium sit amet quis magna.
+					</p>					
+				</div>
+				<hr>
+
+			</div>
+		</div>
+	</div>
+</div>
 
 </body>
 </html>
