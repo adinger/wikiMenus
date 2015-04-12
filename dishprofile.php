23166a6df285afe99cb794d9b46fb7aced8e24fa
aprilx2222 <aprilxu2222@gmail.com>
2015-04-12 18:55:59 -0500

    review and dish profile functional, but ugly

--- dishprofile.php
+++ dishprofile.php
@@ -1,5 +1,90 @@
 <?php
-include "head.php"
+include "head.php";
 require "db/connect.php";
-
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 ?>
+
+<html>
+<h2>
+	<?php 
+		$name = $db->query("SELECT name FROM dish WHERE dishid = $_GET[dish]");
+		$dish = ucwords($name->fetch_assoc()['name']);
+		echo $dish;
+	?>
+</h2>
+<h3>
+	<?php
+		$r = $db->query("SELECT averagerating FROM dish WHERE dishid = $_GET[dish]");
+		$rating = $r->fetch_assoc()['averagerating'];
+		echo $rating;
+		echo " stars<br>";
+		$p = $db->query("SELECT price FROM dish WHERE dishid = $_GET[dish]");
+		$price = $p->fetch_assoc()['price'];
+		echo "$";
+		echo $price;
+		echo " ";
+		$p->free();
+	?>
+	(Tax not included)<div>
+</h3>
+<h3>
+	<?php
+		$t = $db->query("SELECT tagid FROM dishesandtags WHERE dishid = $_GET[dish]");
+		while($row = $t->fetch_assoc()){
+			$tag = $db->query("SELECT tag FROM tags WHERE tagid = $row[tagid]");
+			$tags = $tag->fetch_assoc()['tag'];
+			echo "Tags: ";
+			echo $tags;
+			$tag->free();
+		}
+		$t->free();
+	?>
+</h3>
+<h5>
+	<?php
+		$d = $db->query("SELECT description FROM dish WHERE dishid = $_GET[dish]");
+		$description = $d->fetch_assoc()['description'];
+		echo $description;
+		$d->free();
+	?>
+</h5>
+<form method = "get" action="writereview.php?restaurant ='.$rid. '">
+	<?php 
+		$r = $db->query("SELECT restaurant FROM dish WHERE dishid = $_GET[dish]");
+		$rname = $db->real_escape_string($r->fetch_assoc()['restaurant']);
+		$restaurant = $db->query("SELECT id FROM restaurants WHERE name = '$rname'");
+		$rid = $restaurant->fetch_assoc()['id'];
+		echo "<input type='hidden' name='restaurant' value='".$rid."'>";
+	?>
+    <input type="submit" value="Write a Review">
+</form>
+<br>
+<br>
+<h3>Reviews</h3>
+
+<?php
+	$reviewids= $db->query("SELECT reviewid FROM dishreview WHERE dishid = $_GET[dish]");
+	while($row = $reviewids->fetch_assoc()['reviewid']){
+		echo "<div class='small-9 small-centered columns floating'><fieldset>";
+		$u = $db->query("SELECT useremail FROM dishreview WHERE reviewid = $row");
+		$useremail = $u->fetch_assoc()['useremail'];
+		$useremail = $db->real_escape_string($useremail);
+		$name = $db->query("SELECT username FROM users WHERE email = '$useremail'");
+		$username = $name->fetch_assoc()['username'];
+		echo "<p>Written by ";
+		echo $username;
+		echo "</p>";
+		$rev = $db->query("SELECT * FROM review WHERE reviewid = $row");
+		$review = $rev->fetch_assoc();
+		echo $review['numericalrating'];
+		echo " stars<br><br>";
+		echo $review['verbalreview'];
+		echo "</fieldset></div>";
+		$u->free();
+		$name->free();
+		$rev->free();
+	}
+?>
+
+
+</html?>
--- writereview.php
+++ writereview.php
@@ -2,72 +2,86 @@
 include "head.php";
 require "db/connect.php"; 
 
+if(!isset($_SESSION['username'])){
+	header('Location: loginform.php');
+}
+
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 ?>
 
 <html>
-<h1>Write a Review</h1>
-<fieldset>
-<form method = "post", id = "review">
-	<h3>Review a Dish for 
-		<?php 
-		$name = $db->query("SELECT name FROM restaurants WHERE id = $_GET[restaurant]");
-		$restaurant = ucwords($name->fetch_assoc()['name']);
-		echo $restaurant;
-		$name->free();
-		?>
-	</h3>
-	<label for="reviewdish">Dish Name:</label>
-		<select name="reviewdish" id="reviewdish">
-		<?php
-
-		$dishname = $db->query("SELECT name FROM dish WHERE restaurant = (SELECT name FROM restaurants WHERE id = $_GET[restaurant])");
-		
-		while($row = $dishname->fetch_assoc()){
-			echo "<option value =" .$row['name']. " > " .ucwords($row['name']). " </option>";
-		}
-		$dishname->free();
-		?>
+<center><h2>Write A Review</h1></center>
+	<div class="small-10 small-centered columns floating">
+	<fieldset>
+	<form method = "post", id = "review">
+		<h3>Review a Dish for 
+			<?php 
+			$name = $db->query("SELECT name FROM restaurants WHERE id = $_GET[restaurant]");
+			$restaurant = ucwords($name->fetch_assoc()['name']);
+			echo $restaurant;
+			$name->free();
+			?>
+		</h3>
+		<label for="reviewdish">Dish Name:</label>
+			<select name="reviewdish" id="reviewdish">
+			<?php
+			$dishname = $db->query("SELECT name FROM dish WHERE restaurant = (SELECT name FROM restaurants WHERE id = $_GET[restaurant])");
 			
-		</select>
-	<label for="reviewrating">Your Rating:</label>
-		<select name="reviewrating" id="reviewrating">
-			<option value="5">5</option>
-			<option value="4">4</option>
-			<option value="3">3</option>
-			<option value="2">2</option>
-			<option value="1">1</option>
-		</select>
-	<label for="reviewtext">Your Review:</label>
-		<textarea name="reviewtext" id="reviewtext" rows="10" cols="30">
-		</textarea>
-	<button type = "submit" id="reviewsubmit">Submit Review</button>
-</form>
-</fieldset>
+			while($row = $dishname->fetch_assoc()){
+				echo "<option value ='" .$row['name']. "' > " .ucwords($row['name']). " </option>";
+			}
+			$dishname->free();
+			?>
+				
+			</select>
+		<label for="reviewrating">Your Rating:</label>
+			<select name="reviewrating" id="reviewrating">
+				<option value="5">5</option>
+				<option value="4">4</option>
+				<option value="3">3</option>
+				<option value="2">2</option>
+				<option value="1">1</option>
+			</select>
+		<label for="reviewtext">Your Review:</label>
+			<textarea name="reviewtext" id="reviewtext" rows="10" cols="30">
+			</textarea>
+		<button type = "submit" id="reviewsubmit">Submit Review</button>
+	</form>
+	</fieldset>
 
 <?php
 	if(!empty($_POST)) {
 		if(isset($_POST['reviewdish'], $_POST['reviewrating'], $_POST['reviewtext'])){
-			$dish_name = trim($_POST['reviewdish']);
+			$dish_name = $db->real_escape_string($_POST['reviewdish']);
+			
 			$n = $db->query("SELECT * FROM dish WHERE name = '$dish_name'");
 			$dish_id = $n->fetch_assoc()['dishid'];
+			
 			$dish_rating = trim($_POST['reviewrating']);
-			$dish_review = trim($_POST['reviewtext']);
+			$dish_review = $db->real_escape_string(trim($_POST['reviewtext']));
 			$email = $db->query("SELECT email FROM users WHERE username = '$_SESSION[username]' ");
 			$user_email = $email->fetch_assoc()['email'];
 			if(!empty($dish_name) && !empty($dish_rating) && !empty($dish_review)){
 				$insert = $db->prepare("INSERT INTO review(verbalreview, numericalrating) VALUES (?, ?)");
 				$insert->bind_param('si', $dish_review, $dish_rating);
-				$insert->execute();
-				$review= $db->query("SELECT reviewid FROM review WHERE verbalreview = '$dish_review'");
-				$review_id = $review->fetch_assoc()['reviewid'];
-				$in = $db->prepare("INSERT INTO dishreview(dishid, reviewid, useremail) VALUES(?, ?, ?)");
-				$in->bind_param('iis', $dish_id, $review_id, $user_email);
-				if($in->execute()){
-					header('Location: userprofile.php');
-					die();
+
+				if($insert->execute()){
+
+					$review= $db->query("SELECT reviewid FROM review WHERE verbalreview = '$dish_review'");
+					
+					$review_id = $review->fetch_assoc()['reviewid'];
+					
+					$in = $db->prepare("INSERT INTO dishreview(dishid, reviewid, useremail) VALUES(?, ?, ?)");
+					$in->bind_param('iis', $dish_id, $review_id, $user_email);
+					
+					if($in->execute()){
+						header('Location: userprofile.php');
+						die();
+					}
 				}
 			}
 		}
 	}
 ?>
+<div>
 </html>
