9286ed806d09b3d12c78a23249dfecd163ed92eb
aprilx2222 <aprilxu2222@gmail.com>
2015-04-12 19:06:30 -0500

    asdfad

--- db/connect.php
+++ db/connect.php
@@ -1,7 +1,7 @@
 <?php
 
 error_reporting(E_ALL);
-$db = new mysqli('127.0.0.1', 'root', '', 'wikimenus', 8000);
+$db = new mysqli('127.0.0.1', 'root', '', 'wikimenus');
 if ($db->connect_errno) {
 	echo $db->connect_error;
 	die('Sorry, we are having problems.');
