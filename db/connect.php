33fa838fc4b4b2d3f8801ea4b3452a078fab3a71
aprilx2222 <aprilxu2222@gmail.com>
2015-04-15 13:00:41 -0500

    kfkuk

--- db/connect.php
+++ db/connect.php
@@ -4,7 +4,7 @@ error_reporting(E_ALL);
 // Use following line for cpanel:
 // $db = new mysqli('engr-cpanel-mysql.engr.illinois.edu', 'alding2_user1', 'cs411sp2015', 'alding2_wikimenus2');
 // Use following line for localhost database:
-$db = new mysqli('127.0.0.1', 'root', '', 'wikimenus');
+$db = new mysqli('127.0.0.1', 'root', '', 'wikimenus', 8000);
 if ($db->connect_errno) {
 	echo $db->connect_error;
 	die('Sorry, we are having problems.');
