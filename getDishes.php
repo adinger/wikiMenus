e8a879336b76b0299166211dd05e5bc5067cb183
Angela Lin Ding <angela.ding3@gmail.com>
2015-04-15 01:39:48 -0500

    fixed danishes getdishes.php

--- getDishes.php
+++ getDishes.php
@@ -12,10 +12,10 @@ require '../db/connect.php'; // The mysql database connection script
 	if($result->num_rows > 0) {
 		 while($row = $result->fetch_assoc()) {
 		 	$arr[] = $row;
-		 	// echo $row['description'];
+		 	echo $row['description'];
 		 }
 	}
 	
 	# JSON-encode the response
-	echo $json_response = json_encode($arr);
+	//echo $json_response = json_encode($arr);
 ?>
