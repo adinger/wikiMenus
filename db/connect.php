5f25ad309781c077fd8f6a5a00ba32d6a061a595
aprilx2222 <aprilxu2222@gmail.com>
2015-04-16 20:03:39 -0500

    sdf

--- db/connect.php
+++ db/connect.php
@@ -2,9 +2,9 @@
 
 error_reporting(E_ALL);
 // Use following line for cpanel:
-// $db = new mysqli('engr-cpanel-mysql.engr.illinois.edu', 'alding2_user1', 'cs411sp2015', 'alding2_wikimenus2');
+//$db = new mysqli('engr-cpanel-mysql.engr.illinois.edu', 'alding2_user1', 'cs411sp2015', 'alding2_wikimenus2');
 // Use following line for localhost database:
-$db = new mysqli('127.0.0.1', 'root', '', 'wikimenus', 8000);
+$db = new mysqli('127.0.0.1', 'root', '', 'wikimenus');
 if ($db->connect_errno) {
 	echo $db->connect_error;
 	die('Sorry, we are having problems.');
